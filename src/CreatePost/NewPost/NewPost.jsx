78db7191b06d0a97a7d4813d3dedf20692452937
metamodernity <j772115@gmail.com>
2023-01-25 20:07:53 +0400

    Date input

--- src/CreatePost/NewPost/NewPost.jsx
+++ src/CreatePost/NewPost/NewPost.jsx
@@ -2,20 +2,37 @@ import React from 'react'
 import Input from '../../UI/Input/Input';
 import TextArea from '../../UI/TextArea/TextArea';
 import ColorSelector from '../../UI/ColorSelector/ColorSelector';
+import DateInput from '../../UI/DateInput/DateInput';
 import s from './NewPost.module.css';
+import CheckBoxes from './Checkboxes/Checkboxes';
 const NewPost = () => {
   return (
-        <div className={s.compnentWrapper}>
+        <form className={s.compnentWrapper}>
             <div className={s.compnentTitle}>
                 <h4>Новый пост</h4>
             </div>
             <div className={s.topLevel}>
-                <Input placeholder = "Заголовок (не обязателен)"/>
-                <ColorSelector />
+                <div className={s.inputTitle}>
+                    <Input placeholder = "Заголовок (не обязателен)"/>
+                </div>
+                <div className={s.ColorSelector}>
+                    <ColorSelector />
+                </div>
             </div>
-            <TextArea />
-            <Input placeholder = "Ссылка на картинку" type="url"/>
-        </div>
+            <div className={s.middleLevel}>
+                <div className={s.textArea}>
+                    <TextArea />
+                </div>
+                <div className={s.inputLink}>
+                    <Input placeholder = "Ссылка на картинку" type="url"/>
+                </div>
+                <div className={s.inputDate}>
+                    <DateInput />
+                    <CheckBoxes />
+                </div>
+            </div>
+            
+        </form>
     
   )
 }
