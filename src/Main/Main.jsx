825cbb6032beb372ab39fa2072ee9c39e8e31ce7
Grigory-F <grishka2500@gmail.com>
2023-02-02 21:28:57 +0400

    awdaw
    awdawd

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -4,7 +4,7 @@ import s from './Main.module.css';
 const Main = () => {
   return (
     <div className={s.mainWrapper}>
-      <PageTitle title = "Главная"/>
+      <PageTitle title = "Главнаяssss"/>
     </div>
   )
 }
